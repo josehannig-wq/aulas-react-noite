1780ffca306eb53ffc0a14ed112f4794ef64e18f
josehannig-wq <jose.hannig@escola.pr.gov.br>
2026-07-28 22:14:17 -0300

    Add files via upload

---
+++ src/aula01.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+class Aula01 extends Component {
+    render() {
+        return (
+
+            
+        )
+    }
+}
