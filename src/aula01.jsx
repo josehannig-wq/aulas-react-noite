0c959ed3f566e5583b7064092f6f255d2df06574
josehannig-wq <jose.hannig@escola.pr.gov.br>
2026-08-04 22:15:24 -0300

    Add files via upload

--- src/aula01.jsx
+++ src/aula01.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+
 class Aula01 extends Component {
     render() {
         return (
-
+               <View>
+                   <Text>Estrutura Padrão</Text>
+               </View>
+               
             
         )
     }
 }
+
+export default Aula01;
---
+++ src/aula02.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+class Aula02 extends Component {
+    render() {
+        return (
+               <View>
+                   <Text>Estrutura Padrão</Text>
+                   <Text style={ styles.textoCentral }>Estrutura Padrão</Text>
+                   <Text style={ styles.textoFinal }>Estrutura Padrão</Text>
+               </View>
+               
+            
+        )
+    }
+}
+
+export default Aula02;
+const styles = StyleSheet.create({
+    textoCentral: {
+        color: 'green',
+        fontSize: 30,
+        backgroundColor: 'red',
+        padding: 15
+    },
+    textoFinal:{
+        backgroundColor: 'blue',
+        color: '#FFFFFF'
+
+    }
+})
