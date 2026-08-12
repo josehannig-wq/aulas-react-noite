c8bb0cdeb439097b939e348a21ec4406685dbb58
josehannig-wq <jose.hannig@escola.pr.gov.br>
2026-08-11 22:15:05 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -2,7 +2,8 @@
 //import Aula01 from "./src/aula02;
 
 //import Aula03 from "./src/aula03";
-import Aula04 from "./src/aula04";
+//import Aula04 from "./src/aula04";
+import Aula05 from "./src/aula05";
 
 
 export default function App() {
@@ -10,7 +11,8 @@ export default function App() {
   //<Aula01 />
   //<Aula02/>
   //<Aula03/>
-  <Aula04/>
+  //<Aula04/>
+    <Aula05/>
   );
 }
 
