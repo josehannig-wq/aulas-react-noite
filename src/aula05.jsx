a3c2bcdca86972d5e768b6fe351d631c73d9c2c6
josehannig-wq <jose.hannig@escola.pr.gov.br>
2026-08-18 22:15:26 -0300

    Add files via upload

--- src/aula05.jsx
+++ src/aula05.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Checkbox } from 'expo-checkbox';
 import {
     View,
     Text,
@@ -8,12 +9,24 @@ import {
     TouchableOpacity,
     } from 'react-native';
 
-
 class Aula05 extends Component {
+
+  //Construtor  - Forma traicional de inicializar estado (dados)
+  constructor(props){
+    super(props);
+
+    this.state = {
+      nome:'',
+      email:'',
+      lembrarSenha: false
+    };
+
+  }
   render() {
     return (
       <View style={styles.container}>
         <View>
+          <Text  style={{fontSize: 50, color: '#39FF14', textAlign: 'center', fontWeight: 'bold'}}>LOGIN</Text>
             <Image
                 source={ require('../img/logo-barao.png')}
                 style={ styles.logo}
@@ -31,10 +44,25 @@ class Aula05 extends Component {
             placeholder=' Informe seu e-mail: '
         />
 
-        <TouchableOpacity style={ styles.botao}>
+
+        <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+            <View style= {{ flex: 1, flexDirection: 'row'}}>
+              <Checkbox
+                value={ this.state.lembrarSenha }
+                onValueChange={(valor) => this.setState({ lembrarSenha: valor})}
+                color={ this.state.lembrarSenha ? '#4630EB': undefined}
+                />
+                <Text style={{ color: 'white', marginLeft: 3, fontSize: 12}}>Lembrar Senha? </Text>
+            </View>
+            <Text style={{ color: '#39FF14', fontSize: 12}}>Esqueceu a Senha?</Text>
+        </View>
+
+        <TouchableOpacity style={ styles.botao}> 
             <Text style={ styles.textoBotao}>Entrar</Text>
         </TouchableOpacity>
-        <Text style={ styles.texto1}>Naão tem conta?
+
+
+        <Text style={ styles.texto1}>Não tem conta?
             <Text style={styles.texto2}>Cadastre-se!</Text>
         </Text>
 
@@ -53,9 +81,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'black'
-  },
-
-
+    
   input:{
     width: 250,
     borderWidth: 2,
@@ -91,6 +117,22 @@ const styles = StyleSheet.create({
   },
   textoBotao: {
     color: 'white',
-    textAlign: 'center'
-  }
+    textAlign: 'center'   
+  },
+  texto1: {
+    color: 'white',
+    fontSize: 15,
+    marginTop: 6,
+    textAlign: 'center',
+    
+  },
+
+  texto2: {
+    color: '#39FF14',
+    //marginLeft: 20,
+    fontSize: 15,
+    marginLeft: 20,    
+  
+  
+ },
 });
